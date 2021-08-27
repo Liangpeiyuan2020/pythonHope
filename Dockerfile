7e187e01a52e5665a7924ba07f7e4c1f6409e6cc
梁 <1227689971@qq.com>
2021-08-27 20:46:54 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Use https://hub.docker.com/r/tiangolo/uwsgi-nginx-flask/
+# hello world demo
+FROM python:3.6
+
+#代码添加到code文件夹
+ADD . /code
+# 设置code文件夹是工作目录
+WORKDIR /code
+# 安装支持
+COPY ./app /app
+RUN pip install -r requirements.txt
+CMD ["python", "/app/main.py"]
---
+++ app/main.py
@@ -0,0 +1,37 @@
+import os,demo
+"""
+certifi==2021.5.30
+charset-normalizer==2.0.4
+click==8.0.1
+colorama==0.4.4
+
+Flask==2.0.1
+idna==3.2
+importlib-metadata==4.7.1
+itsdangerous==2.0.1
+Jinja2==3.0.1
+MarkupSafe==2.0.1
+requests==2.26.0
+typing-extensions==3.10.0.0
+urllib3==1.26.6
+Werkzeug==2.0.1
+zipp==3.5.0
+
+"""
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    try:
+        sttr = demo.main2()
+        saar=sttr[o]
+        saar=str(saar)
+        return saar
+    except Exception as e:
+        return e
+    return "uuuuuuuuuuuuuuu"
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
